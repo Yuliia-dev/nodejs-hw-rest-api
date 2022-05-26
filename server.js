3b7a03b2bbffb43699710026a5d0bf05ea985d56
Yuliia <84233109+Yuliia-dev@users.noreply.github.com>
2022-05-26 13:54:00 +0300

    fix port

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const { PORT = 3030 } = process.env;
 
 app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: 3000");
+  console.log(`Server running. Use our API on port: ${PORT}`);
 });
